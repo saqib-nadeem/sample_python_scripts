42ac7fecbb83a19281f87ed43239fd3cb322430d
Saqib Nadeem <saqib598@gmail.com>
2020-02-09 14:00:36 +0500

    Update settings.py

--- angular_flask/settings.py
+++ angular_flask/settings.py
@@ -1,12 +1,12 @@
 #--------Flask Config------------
 DEBUG = True
-SECRET_KEY = 'b7614af64f2f1c6fce84408fd05c946c6426fcdd03b48b60'
+SECRET_KEY = '' # read it from env variables
 
 #--------DB Config------------
 MONGODB_SETTINGS = {
 	'DB':  'meraq',
-	'USERNAME': 'hassan',
-	'PASSWORD': 'hello1234',
-	'HOST': 'ds045011.mongolab.com',
+	'USERNAME': 'saqib',
+	'PASSWORD': '', # read it from env variables
+	'HOST': 'localhost',
 	'PORT': 45011
 }
